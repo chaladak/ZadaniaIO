f5c14867835022c95a468df46fd0d815865e1279
chaladak <117778035+chaladak@users.noreply.github.com>
2023-10-26 11:07:01 +0200

    rejestracja - kod

--- main.cpp
+++ main.cpp
@@ -1,32 +1,83 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    int AA[10];
-    int ujemne;
-    int pominiete;
-    int* BB = new int;
-    int j = 0;
-    for (int i = 0; i < 10; i++) {
-        cout << "wprowadz liczbe: "<< endl;
-        cin >> AA[i];
-        if(AA[i] < 0) {
-            ujemne++;
-            BB[j] = AA[i];
-            j++;
-         }
-         else {
-            pominiete++;
-         }
-            
-    }
-    cout << "Liczb pominietych:" << pominiete <<endl;
-    cout << "Liczb ujemnych: " << ujemne <<endl;
-    cout << "Liczby ujemne: ";
-    for (j=j-1;j !=-1; j--) {
-        cout << BB[j] << "  ";
-    }
-    return 0;
+class User {
+public:
+    string username;
+    string password;
+    string email;
+    string name;
+    string surname;
+    string adres;
+    int id_number;
 
+    User(string uname, string pwd, string mail, string nam, string surnam, string adr, int id) {
+        username = uname;
+        password = pwd;
+        email = mail;
+        name = nam;
+        surname = surnam;
+        id_number = id;
+        adres = adr;
+    }
+
+    void displayUser() {
+        cout << "Username: " << username << endl;
+        cout << "Password: " << password << endl;
+        cout << "Name: " << name << endl;
+        cout << "Surname: " << surname << endl;
+        cout << "ID: " << id_number << endl;
+        cout << "Adres: " << adres << endl;
+    }
+    
+};
+
+User insertData() {
+    vector<User> users; // Lista przechowująca użytkowników
+    string username, password, email, name, surname, adres;
+    int id;
+    cout << "Podaj imie: ";
+    cin >> name;
+
+    cout << "Podaj nazwisko: ";
+    cin >> surname;
+
+    cout << "Podaj PESEL: ";
+    cin >> id;
+
+    cout << "Podaj adres: ";
+    cin >> adres;
+
+    cout << "Podaj adres e-mail: ";
+    cin >> email;
+    
+    cout << "Podaj nazwę użytkownika: ";
+    cin >> username;
+
+    cout << "Podaj hasło: ";
+    cin >> password;
+    
+
+    User newUser(username, password, email,name, surname,adres,id);
+    return newUser;
+
+    for (const auto& user : users) {
+        if (user.username == username) {
+            cout << "Użytkownik o podanej nazwie już istnieje." << endl;
+            break; //break
+        }
+    }
+}
+
+
+int main() {
+    User user = insertData();
+    
+    cout << "Użytkownik zarejestrowany. Szczegóły konta:" << endl;
+    user.displayUser();
+
+    return 0;
 }
