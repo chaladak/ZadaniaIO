0cd36a5eb0006ca78d3628ca3a76a2d744c201ab
chaladak <117778035+chaladak@users.noreply.github.com>
2023-10-30 10:54:16 +0100

    rejestracja do systemu z bazą danych

--- main.cpp
+++ main.cpp
@@ -1,82 +1,181 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 
+const int MaxWords = 100;
+
 class User {
 public:
-    string username;
-    string password;
-    string email;
     string name;
     string surname;
     string adres;
-    int id_number;
-
-    User(string uname, string pwd, string mail, string nam, string surnam, string adr, int id) {
-        username = uname;
-        password = pwd;
-        email = mail;
+    string id_number;
+    string email;
+    string username;
+    string password;
+    
+    User(string nam, string surnam, string adr, string id,  string mail, string uname, string pwd) {
         name = nam;
         surname = surnam;
         id_number = id;
         adres = adr;
+        username = uname;
+        password = pwd;
+        email = mail;  
+    }
+    User() {
+        name = "";
+        surname = "";
+        id_number = "";
+        adres = "";
+        username = "";
+        password = "";
+        email = "";
     }
 
     void displayUser() {
-        cout << "Username: " << username << endl;
-        cout << "Password: " << password << endl;
-        cout << "Name: " << name << endl;
-        cout << "Surname: " << surname << endl;
-        cout << "ID: " << id_number << endl;
+        cout << "----------------------------------------------\nSzczegóły konta:" << endl;
+        cout << "Imie: " << name << endl;
+        cout << "Nazwisko: " << surname << endl;
+        cout << "PESEL: " << id_number << endl;
         cout << "Adres: " << adres << endl;
+        cout << "Nazwa uzytkownika: " << username << endl;
+        cout << "Adres email: " << email << endl;
     }
-    
+
+    void saveToFile(ofstream &file) {
+        file << name << ", " << surname << ", " << id_number << ", " << adres << ", " << email << ", " << username << ", " << password  << endl;
+    }
 };
 
+bool validateID(const string &id) {
+    return id.length() == 11 && id.find_first_not_of("0123456789") == string::npos;
+}
+
+bool validatePassword(const string &password) {
+    return password.length() >= 8;
+}
+
+bool validateUsername(const string &username) {
+    return username.length() >= 6;
+}
+
+bool validateEmail(const string &email) {
+    return email.find('@') != string::npos;
+}
+
+bool validateLength(const string &text, int maxLength) {
+    return text.length() <= maxLength;
+}
+
+
+vector<User> UploadAllUsers() {
+    vector<User> UserData;
+    User buff;
+    const string filename = "C:\\Users\\alicj\\OneDrive\\Pulpit\\projektIO\\dane_pacjentow.txt";
+    
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        cout << "Nie mozna otworzyc pliku." << endl;
+        exit(0); 
+    }
+    else {
+        while (UserData.size() < MaxWords && file >> buff.email >> buff.password) {
+        UserData.push_back(buff); 
+    }
+        file.close();
+    }
+    return UserData;
+}
+
+
 User insertData() {
-    vector<User> users; // Lista przechowująca użytkowników
-    string username, password, email, name, surname, adres;
-    int id;
+
+    vector<User> users = UploadAllUsers();
+    string username, password, email, name, surname, adres, id;
+
+    cout << "\n\nWprowadz dane: " <<endl;
+    
     cout << "Podaj imie: ";
-    cin >> name;
+    getline(cin, name);
+    if (!validateLength(name, 40)) {
+        cout << "Bledna długość imienia." << endl;
+        insertData();
+    }
 
     cout << "Podaj nazwisko: ";
-    cin >> surname;
+    getline(cin, surname);
+    if (!validateLength(surname, 40)) {
+        cout << "Bledna długość nazwiska." << endl;
+        insertData();
+    }
 
     cout << "Podaj PESEL: ";
-    cin >> id;
+    getline(cin, id);
+    if (!validateID(id)) {
+        cout << "Błędny PESEL. PESEL powinien zawierać 11 cyfr." << endl;
+        insertData();
+    }
 
-    cout << "Podaj adres: ";
-    cin >> adres;
+    cout << "Podaj adres zamieszkania: ";
+    getline(cin, adres);
 
     cout << "Podaj adres e-mail: ";
-    cin >> email;
-    
-    cout << "Podaj nazwę użytkownika: ";
-    cin >> username;
+    getline(cin, email);
+    if (!validateEmail(email)) {
+        cout << "Błędny adres e-mail." << endl;
+        insertData();
+    }
 
-    cout << "Podaj hasło: ";
-    cin >> password;
-    
+    cout << "Podaj nazwe uzytkownika: ";
+    getline(cin, username);
+    if (!validateUsername(username)) {
+        cout << "Bledna nazwa użytkownika. Nazwa użytkownika powinna zawierać co najmniej 6 znaków." << endl;
+        insertData();
+    }
 
-    User newUser(username, password, email,name, surname,adres,id);
-    return newUser;
+    cout << "Podaj haslo: ";
+    getline(cin, password);
+    if (!validatePassword(password)) {
+        cout << "Błędne hasło. Hasło powinno zawierać co najmniej 8 znaków." << endl;
+        insertData();
+    }
 
-    for (const auto& user : users) {
+
+    User newUser(name, surname, id, adres, email, username, password);
+
+    ofstream file("C:\\Users\\alicj\\OneDrive\\Pulpit\\projektIO\\dane_pacjentow.txt", ios::app);
+    if (file.is_open()) {
+        newUser.saveToFile(file);
+        file.close();
+    } else {
+        cout << "Nie można otworzyć pliku do zapisu." << endl;
+        exit(0);
+    }
+
+    for (const auto &user : users) {
         if (user.username == username) {
             cout << "Użytkownik o podanej nazwie już istnieje." << endl;
-            break; //break
+            insertData();
+        }
+        if (user.id_number == id) {
+            cout << "Użytkownik o podanym numerze PESEL już istnieje." << endl;
+            insertData();
         }
     }
+    cout << "\n\nUzytkownik zostal poprawnie zarejestrowany.";
+    return newUser;
 }
 
 
 int main() {
+    cout << "\n---------------------------------------------------\nZAKLADANIE KONTA W SYSTEMIE\n---------------------------------------------------" << endl;
     User user = insertData();
-    
-    cout << "Użytkownik zarejestrowany. Szczegóły konta:" << endl;
     user.displayUser();
 
     return 0;
