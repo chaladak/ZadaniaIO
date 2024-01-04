7709b652f30f306ceef82c7049944fe418643642
chaladak <117778035+chaladak@users.noreply.github.com>
2024-01-04 11:03:27 +0100

    zadanie budowniczy

--- main1.cpp
+++ main1.cpp
@@ -1,32 +1,98 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
+// Product class
+class Computer {
+public:
+    void setCPU(const std::string& cpu) {
+        cpu_ = cpu;
+    }
+
+    void setMemory(const std::string& memory) {
+        memory_ = memory;
+    }
+
+    void setStorage(const std::string& storage) {
+        storage_ = storage;
+    }
+
+    void display() {
+        std::cout << "CPU: " << cpu_ << std::endl;
+        std::cout << "Memory: " << memory_ << std::endl;
+        std::cout << "Storage: " << storage_ << std::endl;
+    }
+
+private:
+    std::string cpu_;
+    std::string memory_;
+    std::string storage_;
+};
+
+// Builder interface
+class ComputerBuilder {
+public:
+    virtual void buildCPU(const std::string& cpu) = 0;
+    virtual void buildMemory(const std::string& memory) = 0;
+    virtual void buildStorage(const std::string& storage) = 0;
+    virtual Computer getResult() = 0;
+};
+
+// Concrete Builder
+class DesktopComputerBuilder : public ComputerBuilder {
+public:
+    DesktopComputerBuilder() {
+        computer_ = Computer();
+    }
+
+    void buildCPU(const std::string& cpu) override {
+        computer_.setCPU(cpu);
+    }
+
+    void buildMemory(const std::string& memory) override {
+        computer_.setMemory(memory);
+    }
+
+    void buildStorage(const std::string& storage) override {
+        computer_.setStorage(storage);
+    }
+
+    Computer getResult() override {
+        return computer_;
+    }
+
+private:
+    Computer computer_;
+};
+
+// Director
+class ComputerAssembler {
+public:
+    Computer assembleComputer(ComputerBuilder& builder) {
+        builder.buildCPU("Intel i7");
+        builder.buildMemory("16GB");
+        builder.buildStorage("512GB SSD");
+        return builder.getResult();
+    }
+
+    Computer assembleFastComputer(ComputerBuilder& builder) {
+        builder.buildCPU("Intel i5");
+        builder.buildMemory("8GB");
+        builder.buildStorage("256GB SSD");
+        return builder.getResult();
+    }
+};
 
 int main() {
-    int AA[10];
-    int ujemne;
-    int pominiete;
-    int* BB = new int;
-    int j = 0;
-    for (int i = 0; i < 10; i++) {
-        cout << "wprowadz liczbe: "<< endl;
-        cin >> AA[i];
-        if(AA[i] < 0) {
-            ujemne++;
-            BB[j] = AA[i];
-            j++;
-         }
-         else {
-            pominiete++;
-         }
-            
-    }
-    cout << "Liczb pominietych:" << pominiete <<endl;
-    cout << "Liczb ujemnych: " << ujemne <<endl;
-    cout << "Liczby ujemne: ";
-    for (j=j-1;j !=-1; j--) {
-        cout << BB[j] << "  ";
-    }
-    return 0;
+    DesktopComputerBuilder desktopBuilder;
+    ComputerAssembler assembler;
+    
+    Computer desktop = assembler.assembleComputer(desktopBuilder);
+    std::cout << "Desktop Computer Configuration:" << std::endl;
+    desktop.display();
 
+    Computer fastDesktop = assembler.assembleFastComputer(desktopBuilder);
+    std::cout << "\nFast Desktop Computer Configuration:" << std::endl;
+    fastDesktop.display();
+
+    return 0;
 }
